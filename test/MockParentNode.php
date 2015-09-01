cd1752f77e8b7bdbe72d31074a2a0ba348210560
Eric Mugerwa <eric@rocketphp.io>
2015-09-01 19:30:44 +0100

    Add class documentation to tests

--- test/MockParentNode.php
+++ test/MockParentNode.php
@@ -1,5 +1,16 @@
-<?php namespace RocketPHPTest\HTML;
+<?php
+/**
+ * RocketPHP (http://rocketphp.io)
+ *
+ * @package   RocketPHP
+ * @link      https://github.com/rocketphp/html
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+namespace RocketPHPTest\HTML;
+
 use RocketPHP\HTML\Node_Abstract;
+
 use RocketPHP\HTML\ParentNodeTrait;
 
 class MockParentNode
--- test/functional/HTML.php
+++ test/functional/HTML.php
@@ -1,6 +1,16 @@
-<?php 
+<?php
+/**
+ * RocketPHP (http://rocketphp.io)
+ *
+ * @package   RocketPHP
+ * @link      https://github.com/rocketphp/html
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
 namespace RocketPHPTest\HTML;
+
 use RocketPHP\HTML\HTML;
+
 /**
  * @group RocketPHP_HTML
  */ 
